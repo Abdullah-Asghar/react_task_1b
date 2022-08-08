e63af0116c9b40f605599dda0962c4edd594de7a
Abdullah Asghar <abdullahmasghar1995@gmail.com>
2022-08-09 00:20:38 +0500

    Point 1: Login method

--- src/utils/MkdSDK.jsx
+++ src/utils/MkdSDK.jsx
@@ -12,9 +12,38 @@ export default function MkdSDK() {
   this.setTable = function (table) {
     this._table = table;
   };
-  
+
   this.login = async function (email, password, role) {
     //TODO
+    const header = {
+      "Content-Type": "application/json",
+      "x-project": base64Encode,
+    };
+
+    const getResult = await fetch(
+      this._baseurl + `/v2/api/lambda/login`,
+      {
+        method: "POST",
+        headers: header,
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          role: role,
+        }),
+      }
+    );
+
+    const jsonGet = await getResult.json();
+
+    if (getResult.status === 401) {
+      throw new Error(jsonGet.message);
+    }
+
+    if (getResult.status === 403) {
+      throw new Error(jsonGet.message);
+    }
+
+    return jsonGet;
   };
 
   this.getHeader = function () {
@@ -27,7 +56,7 @@ export default function MkdSDK() {
   this.baseUrl = function () {
     return this._baseurl;
   };
-  
+
   this.callRestAPI = async function (payload, method) {
     const header = {
       "Content-Type": "application/json",
@@ -55,7 +84,7 @@ export default function MkdSDK() {
           throw new Error(jsonGet.message);
         }
         return jsonGet;
-      
+
       case "PAGINATE":
         if (!payload.page) {
           payload.page = 1;
@@ -84,7 +113,7 @@ export default function MkdSDK() {
       default:
         break;
     }
-  };  
+  };
 
   this.check = async function (role) {
     //TODO
